0c2594945c711974fb46725e11a4042484a1db37
sktzwhj <whj_nudt@foxmail.com>
2017-09-22 16:16:50 +1000

    add Q287

--- Q287_Find_the_Duplicate_Number.cpp
+++ Q287_Find_the_Duplicate_Number.cpp
@@ -2,3 +2,22 @@
 // Created by wu061 on 22/09/17.
 //
 
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int last_num = 0;
+        for(vector<int>::iterator it = nums.begin();it!=nums.end();it++)
+        {
+            if(*it==last_num)
+                return last_num;
+            else
+                last_num = *it;
+        }
+
+    }
+};
