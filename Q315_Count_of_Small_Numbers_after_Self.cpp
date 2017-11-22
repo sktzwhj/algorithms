cfe249a22baa4a4eac3e2cc007bb97e5f86b163f
sktzwhj <whj_nudt@foxmail.com>
2017-11-23 08:44:57 +1100

    just make the indices go with the nums so that we do not need to track

--- Q315_Count_of_Small_Numbers_after_Self.cpp
+++ Q315_Count_of_Small_Numbers_after_Self.cpp
@@ -13,55 +13,89 @@ public:
 
         printVector(nums);
         vector<int> smallerResult = vector<int>(nums.size(),0);
-        mergeFindSmaller(nums, 0,  smallerResult);
+        vector<int> indices = vector<int>(nums.size(),0);
+        for(int k = 0; k < nums.size(); k++)
+            indices[k] = k;
+        mergeFindSmaller(nums, 0, nums.size() -1,indices, smallerResult);
         printVector(nums);
         //cout<<"smallerResult"<<endl;
         printVector(smallerResult);
+        cout<<"indices:"<<endl;
+        printVector(indices);
         return smallerResult;
     }
 
-    void mergeFindSmaller(vector<int>& nums, int start,  vector<int>& smallerResult){
+    void mergeFindSmaller(vector<int>& nums, int start, int end, vector<int>&indices, vector<int>& smallerResult){
+        cout<<"start:"<<start<<"num len:"<<nums.size()<<endl;
+
         if(nums.size() < 2)
             return;
-        int mid = (nums.size() - 1)/2;
+        int mid = (start + end )/2;
         vector<int> L = vector<int>();
         vector<int> R = vector<int>();
-        for(int i = 0; i <= mid; i++)
-            L.push_back(nums[i]);
-        for(int j = mid + 1; j < nums.size(); j++)
-            R.push_back(nums[j]);
+        for(int i = start; i <= mid; i++)
+            L.push_back(nums[i - start]);
+        for(int j = mid + 1; j <= end; j++)
+            R.push_back(nums[j - start]);
         //printVector(L);
         //printVector(R);
-        mergeFindSmaller(L, start,smallerResult);
-        mergeFindSmaller(R, mid+1, smallerResult);
-        merge(L,R,nums, start, mid, smallerResult);
+        mergeFindSmaller(L, start, mid, indices,smallerResult);
+        mergeFindSmaller(R, mid+1, end, indices, smallerResult);
+        merge(L,R,nums, start, end, indices, smallerResult);
     }
 
 private:
-    void merge(vector<int> L, vector<int> R, vector<int>& nums, int start, int mid,  vector<int>& smallerResult) {
-        int i = 0, j = 0, index = 0;
-        while (i <  L.size() && j < R.size() ) {
-            if (L[i] < R[j]) {
-                nums[index++] = L[i++];
+    void merge(vector<int> L, vector<int> R, vector<int>& nums, int start, int end, vector<int>& indices, vector<int>& smallerResult) {
+        int mid = (start+end)/2;
+
+        int i = start, j = mid + 1, index = 0;
+
+
+
+        cout<<"start "<<start<<"end "<<end<<endl;
+        while (i <= mid && j <= end ) {
+            if (L[i - start] < R[j - mid -1]) {
+                indices[start + index] = i;
+                nums[index++] = L[i - start];
+
+                i++;
             }
-            else if(R[j] < L[i]) {
-                nums[index++] = R[j++];
-                for(int k = i; k < L.size() ; k++)
-                    smallerResult[start + k]++;
+            else if(R[j - mid - 1] < L[i - start]) {
+                indices[start + index] = j;
+                nums[index++] = R[j - mid - 1];
+                cout<<"nums["<<j<<"]< nums["<<i<<"]"<<endl;
+                for(int k = i; k <= mid ; k++){
+                    smallerResult[indices[k]]++;
+                }
+                j++;
+
+
+
             }
             else {
                 //if the two are equal
-                nums[index++] = L[i++];
-                nums[index++] = R[j++];
+                indices[start + index] = i;
+                nums[index++] = L[i - start];
+                indices[start + index] = j;
+                nums[index++] = R[j - mid - 1];
+                i++;
+                j++;
             }
 
         }
 
-        while (i < L.size())
-            nums[index++] = L[i++];
+        while (i <= mid){
+            indices[start + index] = i;
+            nums[index++] = L[i - start];
+            i++;
+        }
 
-        while (j < R.size())
-            nums[index++] = R[j++];
+        while (j <= end){
+            indices[start + index] = j;
+            nums[index++] = R[j - mid - 1];
+            j++;
+        }
+
     }
 
     void printVector(vector<int> nums)
@@ -74,8 +108,8 @@ private:
 };
 
 int main(){
-    int a[] = {5,2,6,1};
-    vector<int> input = vector<int>(a,a+4);
+    int a[] = {2,0,1};
+    vector<int> input = vector<int>(a,a+3);
     Solution s = Solution();
     s.countSmaller(input);
 }
