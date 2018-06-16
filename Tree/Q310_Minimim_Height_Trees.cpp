f08b4d3bcf65d8947d6c2be513d2517ba7cba9ee
sktzwhj <whj_nudt@foxmail.com>
2018-06-16 23:11:14 +1000

    fix Q310 but got TLE, smarter solution exists

--- Tree/Q310_Minimim_Height_Trees.cpp
+++ Tree/Q310_Minimim_Height_Trees.cpp
@@ -10,55 +10,51 @@
 
 using namespace std;
 
+//first it can be quite intuitive to think about BFS. Unfortunately, got TLE.  
+
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<pair<int, int>> &edges) {
         vector<int> roots;
         unordered_map<int, vector<int>> neighbors;
-        for (int i = 0; i < n; i++) neighbors.insert(make_pair(i, vector<int>()));
         for (int i = 0; i < edges.size(); i++) {
             neighbors[edges[i].first].push_back(edges[i].second);
             neighbors[edges[i].second].push_back(edges[i].first);
         }
-        unordered_map<int, int> root_depth_map;
+        int min_depth = n;
         for (int i = 0; i < n; i++) {
-            int max_depth = 1;
-            set<int> exist_nodes = set<int>();
-            getMaxDepth(i, neighbors, exist_nodes, max_depth);
-            cout << "depth for root " << i << " is" << max_depth << endl;
-            root_depth_map[i] = max_depth;
-        }
-        vector<pair<int, int>> root_depth_vec(root_depth_map.begin(), root_depth_map.end());
-        sort(root_depth_vec.begin(), root_depth_vec.end(), compareByValue);
-        int min_depth = root_depth_vec[0].second;
-        for (auto e:root_depth_vec) {
-            if (e.second == min_depth) roots.push_back(e.first);
+            int max_depth = 0;
+            int layer = 0;
+            vector<int> exist(n, 0);
+            getMaxDepth(i, neighbors, exist, max_depth, layer);
+            if (max_depth == min_depth)
+                roots.push_back(i);
+            else if (max_depth < min_depth) {
+                roots.clear();
+                roots.push_back(i);
+                min_depth = max_depth;
+            }
         }
         return roots;
     }
 
 private:
-    void getMaxDepth(int root, unordered_map<int, vector<int>> &neighbors, set<int> &exist_nodes, int &max_depth) {
-        cout<<"-> "<<root;
-        exist_nodes.insert(root);
+    void getMaxDepth(int root, unordered_map<int, vector<int>> &neighbors, vector<int> &exist, int &max_depth,
+                     int layer) {
+        exist[root] = 1;
         int valid_neighbor_num = 0;
         for (auto neighbor: neighbors[root]) {
-            if (exist_nodes.find(neighbor) == exist_nodes.end()) {
-                valid_neighbor_num++;
-                getMaxDepth(neighbor, neighbors, exist_nodes, max_depth);
+            if (!exist[neighbor]) {
+                if (max_depth < layer + 1) max_depth = layer + 1;
+                getMaxDepth(neighbor, neighbors, exist, max_depth, layer + 1);
             }
         }
-        if (valid_neighbor_num > 0) ++max_depth;
-        else cout<<endl;
-    }
-
-    bool static compareByValue(pair<int, int> &a, pair<int, int> &b) {
-        return a.second < b.second;
     }
 };
 
 int main() {
-    vector<pair<int, int>> edges = {make_pair(0, 1), make_pair(0, 2), make_pair(0, 3), make_pair(3, 4), make_pair(4, 5)};
+    vector<pair<int, int>> edges = {make_pair(0, 1), make_pair(0, 2), make_pair(0, 3), make_pair(3, 4),
+                                    make_pair(4, 5)};
     int n = 6;
     Solution s = Solution();
     vector<int> roots = s.findMinHeightTrees(n, edges);
