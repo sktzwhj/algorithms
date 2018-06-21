64c4a9d383129fb2b71168e646e6dc9054222b77
sktzwhj <whj_nudt@foxmail.com>
2018-06-21 21:58:47 +1000

    add Q513

---
+++ BFS/Q513_Find_Bottom_Left_Tree_Value.cpp
@@ -0,0 +1,51 @@
+//
+// Created by wuhuijun on 6/21/18.
+//
+
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+//the idea is simple, just do BFS and get the rightmost node of each layer
+#define PAIR pair<TreeNode*, int>
+
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode *root) {
+        queue <PAIR> unvisited;
+        unvisited.push(make_pair(root, 1));
+        vector <pair<int, int>> visited;
+        while (!unvisited.empty()) {
+            PAIR curr = (unvisited.front());
+            if (curr.first->left != NULL) unvisited.push(make_pair(curr.first->left, curr.second + 1));
+            if (curr.first->right != NULL) unvisited.push(make_pair(curr.first->right, curr.second + 1));
+            visited.push_back(make_pair(curr.first->val, curr.second));
+            unvisited.pop();
+        }
+        for (int i = visited.size() - 1; i >= 0; i--) {
+            if (visited[i].second > visited[i - 1].second || i == 0) {
+                return visited[i].first;
+            }
+        }
+    }
+};
+
+int main() {
+    TreeNode *t1 = new TreeNode(2);
+    TreeNode *t2 = new TreeNode(1);
+    TreeNode *t3 = new TreeNode(3);
+    t1->left = t2;
+    t1->right = t3;
+    Solution s = Solution();
+    cout << s.findBottomLeftValue(t1) << endl;
+}
